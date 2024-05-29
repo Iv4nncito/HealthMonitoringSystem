fbf3ad225c0652050dc998d6cf88b9dbb73575d9
Iv4nncito <110946812+Iv4nncito@users.noreply.github.com>
2024-05-29 17:09:57 +0300

    add new variables

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 import os
 from dotenv import load_dotenv
 from routes import configure_routes
+
 load_dotenv()
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "a_random_secret_key")
 configure_routes(app)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
