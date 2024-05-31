237db00b5d397d349fe863e7c52b75d4a2bca952
Iv4nncito <110946812+Iv4nncito@users.noreply.github.com>
2024-05-31 12:45:26 +0300

    change variable

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
-from routes import configure_routes
+from routes import configure_api_routes
 
 load_dotenv()
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "a_random_secret_key")
-configure_routes(app)
+health_monitoring_app = Flask(__name__)
+health_monitoring_app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY", "default_secret_key")
+configure_api_routes(health_monitoring_app)
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    server_port = int(os.getenv("SERVER_PORT", 5000))
+    health_monitoring_app.run(host="0.0.0.0", port=server_port)
